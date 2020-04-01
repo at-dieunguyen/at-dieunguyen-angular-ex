7410b90ecf2ef5194f47b68d4b7425542d8c7502
at-dieunguyen <dieu.nguyen@asiantech.vn>
2020-04-02 00:07:25 +0700

    validate pass

--- src/app/shared/layout/form-reactive/form-reactive.component.ts
+++ src/app/shared/layout/form-reactive/form-reactive.component.ts
@@ -18,37 +18,44 @@ export class FormReactiveComponent implements OnInit {
     this.regFrom = this.fb.group({
       fullName: ['', [Validators.required, Validators.minLength(10)]],
       emailAddress: ['', [Validators.required, Validators.email]],
-      pw: this.fb.group({
-        password: ['', Validators.required],
-        confirmPassword: ['', Validators.required]
-      }, {
-        validator: comparePassword
-      }),
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]],
       skills: this.fb.array([
         this.fb.control('')
-      ]
-      )
-    });
+      ], [Validators.required])
+    }, {
+      validators: this.matchPasswords
+    }
+    );
 
   }
 
   onSubmit() {
     console.log(this.regFrom);
   }
+  get skills() {
+    return this.regFrom.get('skills') as FormArray;
+  }
 
   addSkill() {
     this.skills.push(this.fb.control(''));
   }
 
-  get skills(){
-    return this.regFrom.get('skills') as FormArray;
+  matchPasswords(group: FormGroup) {
+    let pass = group.get('password').value;
+    let confirmPass = group.get('confirmPassword').value;
+    return pass === confirmPass ? null : { notSame: true }
   }
-
-}
-
-export function comparePassword(c: AbstractControl) {
-  const v = c.value;
-    return (v.password === v.confirmPassword) ? null : {
-      passwordnotmatch: true
-    };
+  get fullName() {
+    return this.regFrom.get('fullName');
+  }
+  get email() {
+    return this.regFrom.get('emailAddress');
+  }
+  get password() {
+    return this.regFrom.get('password');
+  }
+  get confirmPass() {
+    return this.regFrom.get('confirmPassword');
+  }
 }
