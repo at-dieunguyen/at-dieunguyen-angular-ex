6757b65351727fdd78eb0e3bb55583263587550f
at-dieunguyen <dieu.nguyen@asiantech.vn>
2020-04-09 14:33:40 +0700

    fix some convention

--- deloy.js
+++ deloy.js
@@ -1,8 +1,8 @@
 var ghpages = require('gh-pages');
 
 ghpages.publish('dist/angular-learn', {
-  branch: 'gh-pages',
-  repo: 'https://github.com/at-dieunguyen/test.git'
+  branch: 'master',
+  repo: 'https://github.com/at-dieunguyen/ng-angular-staging.git'
 }, function(e) {
   console.log('deloyed');
 });
